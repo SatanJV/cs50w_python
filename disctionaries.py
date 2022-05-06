acdefeec0474c31489a54d3d5917ffc9c59dc04e
João Vieira <joao.vieira.dev@gmail.com>
2022-05-06 10:00:35 +0100

    Django Project first commit

--- disctionaries.py
+++
@@ -1,3 +0,0 @@
-houses = {"Harry": "Gryffindor", "Draco": "Slytherin"}
-houses["Hermione"] = "Gryffindor"
-print(houses["Hermione"])
--- hello.py
+++
@@ -1,7 +0,0 @@
-number = int(input("Number: "))
-if number < 0 :
-    print(str(number) + " is a negative number")
-elif number > 0:
-    print(str(number) + " is positive")
-else:
-    print(str(number) + " is zero")
