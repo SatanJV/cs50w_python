242836c8cba35e2e0afaaed643d6e01aaa2c2ed6
João Vieira <joao.vieira.dev@gmail.com>
2022-05-06 16:31:42 +0100

    Third commit

--- Django/tasks/views.py
+++ Django/tasks/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
+from django import forms
+
 tasks = ["foo", "bar", "baz"]
+
+class newTaskForm(forms.Form):
+    task = forms.CharField(label ="New Task")
+    priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
+
 # Create your views here.
 
 def index(request):
     return render(request, "tasks/index.html", {
         "tasks": tasks
+    })
+
+def add(request):
+    return render(request, "tasks/add.html", {
+        "form": newTaskForm()
     })
